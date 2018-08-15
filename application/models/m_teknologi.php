0c39cb9a1adb1da40ca6361aa94f128b9bc8dff0
gusna <gusna@GUSNAIKHSAN>
2018-08-15 11:23:27 +0700

    searching part1

--- application/models/m_teknologi.php
+++ application/models/m_teknologi.php
@@ -32,5 +32,10 @@
 			return $hasil[0]->id_teknologi_budidaya;
 		}
 
+		public function pencarianTeknologi($cari){
+			$sql = $this->db->query("SELECT t.id_teknologi_budidaya,t.jenis_teknologi_budidaya,f.id_file,f.nama_file,f.deskripsi_file,f.file FROM file_teknologi f join teknologi_budidaya t on f.id_teknologi_budidaya = t.id_teknologi_budidaya where jenis_teknologi_budidaya = \"$cari\"");
+			return $sql->result_array();
+		}
+
 	} 
 ?>

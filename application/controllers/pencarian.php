5ba581f8f2aef97c7da0c213521289ffdafceee2
gusna <gusna@GUSNAIKHSAN>
2018-08-20 21:34:05 +0700

    conflict solve

--- application/controllers/pencarian.php
+++ application/controllers/pencarian.php
@@ -13,21 +13,6 @@
 			$this->load->model("m_agribisnis");									
 		}
 
-<<<<<<< HEAD
-		public function cari(){
-			$this->load->model("m_teknologi");						
-			$cari = $this->input->post('keyword');		
-			$datapencarian['pencarianTekno'] = $this->m_teknologi->pencarianTeknologi($cari);			
-
-=======
-		// public function index(){
-		// 	$dataHeader['judul'] = "Pencarian";
-			
-		// 	$this->load->view('header', $dataHeader);			
-		// 	$this->load->view('HalamanPencarian');
-		// 	$this->load->view('footer');
-		// }
-
 		public function index(){
 			$cari = $this->input->get('keyword');
 			$datapencarian['keyword'] = $cari;	
@@ -36,7 +21,6 @@
 			$datapencarian['pencarianLeaflet'] = $this->M_leaflet->pencarianLeaflet($cari);
 			$datapencarian['pencarianTekno'] = $this->m_teknologi->pencarianTeknologi($cari);
 			$datapencarian['pencarianAgribisnis'] = $this->m_agribisnis->pencarianAgribisnis($cari);
->>>>>>> d14662d6cd93330899813c3b0dd096cc50b8e856
 			
 			$dataHeader['judul'] = "Pencarian";
 			$this->load->view('header', $dataHeader);			
